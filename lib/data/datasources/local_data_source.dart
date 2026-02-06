bc66ead7f5fb5fa0e3ce87c52c3b8173a08b24c0
haedirasso-star <haedirasso@gmail.com>
2026-02-07 00:05:33 +0300

    Delete lib directory

--- lib/data/datasources/local_data_source.dart
+++
@@ -1,8 +0,0 @@
-import '../models/content_model.dart';
-
-/// Local data source (Hive / Isar / SharedPreferences).
-abstract class LocalDataSource {
-  Future<List<ContentModel>> getContinueWatching();
-  Future<void> saveContinueWatching(List<ContentModel> list);
-  Future<void> clearCache();
-}
